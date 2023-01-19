fec3bd247f81b42616712906c5ca6806e146bfa7
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-01-19 06:44:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.OwnerNumber = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
-global.ownertag = ['436764242454']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.Owner = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.OwnerNumber = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
+global.ownertag = ['4915212908434']  // -- Add multiple numbers like  ['918101967835','919357940276','917896587453']
 global.BotName = "Miku"
-global.packname = "Erstellt für Sebastian"
-global.author = "Sebastian"
-global.OwnerName = "Sebastian"
+global.packname = "Erstellt für ✨️MAGIC✨️"
+global.author = "✨️MAGIC✨️"
+global.OwnerName = "✨️MAGIC✨️"
 global.BotSourceCode = "https://github.com/FantoX001/Miku-MD" // -- Please don't change -- //
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"  // -- Please don't change -- //
 global.sessionName = "session"  // -- Don't change otherwise bot will not work -- //
@@ -28,7 +28,7 @@ global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
 global.websitex = "https://github.com/Kai0071"
-global.lolhuman = "Sebastian"
+global.lolhuman = "✨️MAGIC✨️"
 
 
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
